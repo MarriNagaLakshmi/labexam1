a5e9326c22ef659dfba67dc7fe11adcbdcfdb921
BadhariKesh26 <badharikesh246@gmail.com>
2026-09-24 10:50:01 +0530

    Fix incorrect expected value in count_negatives test

--- test_app.py
+++ test_app.py
@@ -18,12 +18,12 @@ def test_sum_list(numbers, expected):
 def test_count_negatives(numbers, expected):
     assert count_negatives(numbers) == expected
 
-
 @pytest.mark.parametrize("numbers, expected", [
     ([], 0),
     ([1, 2, 3], 0),
     ([-1, 2, -3], 2),
-    ([1, -1], 5)  # <-- Wrong expected value case
+    ([1, -1], 1)  # <-- Changed 5 to 1 to fix the test
 ])
 def test_count_negatives(numbers, expected):
     assert count_negatives(numbers) == expected
+
